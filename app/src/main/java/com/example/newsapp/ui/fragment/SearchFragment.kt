0d53c0d7b5153234413d8735d1b47e5ef8d60b32
Siya <siyagolatkar2002@gmail.com>
2023-09-24 12:45:41 +0530

    version 1.1

---
+++ app/src/main/java/com/example/newsapp/ui/fragment/SearchFragment.kt
@@ -0,0 +1,26 @@
+package com.example.newsapp.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newsapp.R
+
+class SearchFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.news_search, container, false)
+    }
+
+}
